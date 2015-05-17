d93cbb0007d0ef70a96e32e31663a7152041b65a
venkat <venkatmail@gmail.com>
2015-05-17 13:30:58 -0400

    Initial revision..

---
+++ eshoppe/src/main/java/com/training/java/eshoppe/App.java
@@ -0,0 +1,13 @@
+package com.training.java.eshoppe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
